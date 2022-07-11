1395340a442ecc2ffa1b912e3be2c7a0668f8845
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-07-11 19:08:10 +0300

    Добавила условие создания карточки

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -12,26 +12,26 @@ const CardForm = ({create}) => {
       id: Date.now(),
       ...card
     };
-    create(newCard);
+    if (card.title !== '' && card.body !== '') {
+      create(newCard);
+    }
     setCard({ title: '', body: '' });
   }
 
   return (
-    <div>
-      <form>
-        <CardInput
-          type="text"
-          placeholder='Термин'
-          value={card.title}
-          onChange={e => setCard({...card, title: e.target.value })} />
-        <CardInput
-          type="text"
-          placeholder='Определение'
-          value={card.body}
-          onChange={e => setCard({...card, body: e.target.value })} />
-        <Button onClick={addNewCard}>Создать карточку</Button>
-      </form>
-    </div>
+    <form>
+      <CardInput
+        type="text"
+        placeholder='Термин'
+        value={card.title}
+        onChange={e => setCard({...card, title: e.target.value })} />
+      <CardInput
+        type="text"
+        placeholder='Определение'
+        value={card.body}
+        onChange={e => setCard({...card, body: e.target.value })} />
+      <Button onClick={addNewCard}>Добавить</Button>
+    </form>
   );
 };
 
